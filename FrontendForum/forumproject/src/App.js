4a037020b9c778f4c385cca0b32fe2bc7686cb53
Andrea Pollini <andrea9711@hotmail.it>
2021-05-10 17:11:18 +0200

    strutturaproject

---
+++ FrontendForum/forumproject/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Route} from "react-router-dom";
+import {Routes} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="main">
+
+    </div>
+  );
+}
+
+export default App;
