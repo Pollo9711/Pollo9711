74ea12e2aab5010e49143c920694e68def8dd27a
Andrea Pollini <andrea9711@hotmail.it>
2021-05-11 16:35:59 +0200

    implementazioneFooter

--- FrontendForum/forumproject/src/components/layout/Footer/Footer.js
+++ FrontendForum/forumproject/src/components/layout/Footer/Footer.js
@@ -1,7 +1,18 @@
+import "./Footer.css"
+import {faRobot} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+
 const Footer = () => {
 
     return (
         <>
+            <div id={"footer"}>
+                <div id={"logo"}>
+                    <FontAwesomeIcon icon={faRobot}/>
+                </div>
+                <p>Copyright ©</p>
+            </div>
 
         </>
     );
