b3a8f827d5cd2264398a9d4b03dbc0734746d6ff
Massimiliano De Crignis <massimilianodecrignis@gmail.com>
2021-05-11 15:30:23 +0200

    done registration

--- FrontendForum/forumproject/src/components/core/Registrati/Registrati.js
+++ FrontendForum/forumproject/src/components/core/Registrati/Registrati.js
@@ -1,8 +1,49 @@
+import {useState} from "react";
+
+// NB CSS DA FARE
+
 const Registrati = () => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const [formValue, setFormValue] = useState({
+        username: '',
+        password: ''
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(`Username: ${formValue.username} | Password: ${formValue.password}`)
+    }
+
+    const handleChange = (e) => {
+        const user = e.target.name;
+        console.log(user);
+        setFormValue({
+            ...formValue,
+            [user]: e.target.value
+        })
+    }
 
     return (
         <>
+            <form onSubmit={handleSubmit}>
+                <input type="text"
+                       placeholder={'Username'}
+                       name={'username'}
+                       value={formValue.username}
+                       onChange={handleChange}
+                />
 
+                <input type="text"
+                       placeholder={'Password'}
+                       name={'password'}
+                       value={formValue.password}
+                       onChange={handleChange}
+                />
+
+                <button type={'submit'}>Submit</button>
+            </form>
         </>
     );
 }
